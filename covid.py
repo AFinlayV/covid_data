24a4da0829c81c5b35f3b1bb20362dc19056bebb
AFinlayV <60828309+AFinlayV@users.noreply.github.com>
2020-07-03 15:56:50 -0400

    Update covid.py

--- covid.py
+++ covid.py
@@ -1,6 +1,7 @@
 import pandas as pd
+import urllib
 
-def load_data:
+def load_data():
     url = f'https://covidtracking.com/api/v1/states/daily.json'
     uh = urllib.request.urlopen(url)
     data = uh.read().decode()
@@ -20,5 +21,6 @@ def load_data:
     data_cur = pd.read_json('covid_data_current.json')
     data_day = pd.read_json('covid_data_daily.json')
     state_pop = pd.read_csv('SCPRC-EST2019-18+POP-RES.csv')
+    state_abrev = pd.read_json('state_abrev.json')
 
-    return {'current_data':data_cur, 'daily_data':data_day, 'state_data':state_pop}
+    return {'current_data':data_cur, 'daily_data':data_day, 'state_data':state_pop, 'state_abrev':state_abrev}
